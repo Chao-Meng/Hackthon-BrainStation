51a53e44abce856bf2160b7904addbc779bdd7cc
Chao Meng <cmeng036@gmail.com>
2024-01-10 16:58:00 -0500

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+function createDiv(className) {
+  let divName = document.createElement("div");
+  divName.classList.add(className);
+  return divName;
+}
+function createImg(className) {
+  let imgName = document.createElement("img");
+  imgName.classList.add(className);
+  return imgName;
+}
